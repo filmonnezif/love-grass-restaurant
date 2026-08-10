8d05563c6f689c3d1ac724fa8a1e4967c6dd0ae0
FILMON NEZIF <filmonnezif@users.noreply.github.com>
2026-08-11 02:48:33 +0400

    fix: use canonical production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://love-grass.com',
+  site: 'https://www.lovegrassdubai.com',
   integrations: [sitemap()],
   image: {
     domains: [],
